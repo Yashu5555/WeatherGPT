a96ff2610d71408692d00f2f8760a06013ad6359
firend gjc <friendlysonu10@gmail.com>
2026-09-13 19:02:36 +0530

    database queries

--- database/queries.sql
+++ database/queries.sql
@@ -2,15 +2,15 @@
 INSERT INTO users (user_name, email, native_lang, preferences)
 VALUES ('Test User', 'test@example.com', 'English', 'Temperature alerts');
 
-SELECT * FROM users;
+-- SELECT * FROM users;
 
 INSERT INTO weather_queries (user_id,question,location,at_what_time)
-values(1,"Should I carry an umbrella tomorrow evening in Hyderabad?","hyderabad",current_timestamp());
+VALUES(1,"Should I carry an umbrella tomorrow evening in Hyderabad?","hyderabad",current_timestamp());
 
-SELECT * FROM weather_queries;
+-- SELECT * FROM weather_queries;
 
-SELECT * FROM weather_history;
+-- SELECT * FROM weather_history;
 
-SELECT * FROM alerts;
+-- SELECT * FROM alerts;
 
-SELECT * FROM advisories;
+-- SELECT * FROM advisories;
